0dbd812657023e70d703684d7bcc2a85597794ac
Kai120789 <zoomer120789@gmail.com>
2024-12-09 16:20:24 +0300

    feat: class component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, {useState} from "react";
 import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
   const [value, setValue] = useState('TEXT')
 
   return (
     <div className="App">
-      <Counter/>
-      <Counter/>
-      <Counter/>
+
+      <ClassCounter/>
     </div>
   );
 }
