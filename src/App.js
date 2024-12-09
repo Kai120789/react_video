611a0554ebfd395ad908614b6247dd1006eca790
Kai120789 <zoomer120789@gmail.com>
2024-12-09 16:30:45 +0300

    feat: css styles and props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import './styles/App.css'
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 
@@ -7,8 +8,41 @@ function App() {
 
   return (
     <div className="App">
+      <div className="post">
+        <div className="post_content">
+          <strong>1. Golang</strong>
+          <div>
+            Golang - тема
+          </div>
+        </div>
+        <div className="post_btns">
+          <button>delete</button>
+        </div>
+      </div>
 
-      <ClassCounter/>
+      <div className="post">
+        <div className="post_content">
+          <strong>1. Golang</strong>
+          <div>
+            Golang - тема
+          </div>
+        </div>
+        <div className="post_btns">
+          <button>delete</button>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="post_content">
+          <strong>1. Golang</strong>
+          <div>
+            Golang - тема
+          </div>
+        </div>
+        <div className="post_btns">
+          <button>delete</button>
+        </div>
+      </div>
     </div>
   );
 }
