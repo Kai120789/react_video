e8bdbef10b9b2c9e31c291a434374cfd00bac259
Kai120789 <zoomer120789@gmail.com>
2024-12-11 19:44:37 +0300

    feat: add post list and map struct for posts

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import './styles/App.css'
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
-  const [value, setValue] = useState('TEXT')
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Golang', body: 'Description'},
+    {id: 2, title: 'Python', body: 'Description'},
+    {id: 3, title: 'JavaScript', body: 'Description'}
+  ])
+
+  const [posts2, setPosts2] = useState([
+    {id: 1, title: 'С++', body: 'Description'},
+    {id: 2, title: 'С#', body: 'Description'},
+    {id: 3, title: 'Java', body: 'Description'}
+  ])
 
   return (
     <div className="App">
-        <PostItem post={{id: 1, title: 'Golang', body: 'Description'}}/>
-        <PostItem post={{id: 2, title: 'Python', body: 'Description'}}/>
-        <PostItem post={{id: 3, title: 'JavaScript', body: 'Description'}}/>
+        <PostList posts={posts} title='Список постов'/>
+        <PostList posts={posts} title='Список постов2'/>
     </div>
   );
 }
