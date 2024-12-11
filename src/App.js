ec6f3cee2be1b3eed510099cad58c537c82db04c
Kai120789 <zoomer120789@gmail.com>
2024-12-11 19:26:25 +0300

    feat: props arguments

--- src/App.js
+++ src/App.js
@@ -2,47 +2,16 @@ import React, {useState} from "react";
 import './styles/App.css'
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
+import PostItem from "./components/PostItem";
 
 function App() {
   const [value, setValue] = useState('TEXT')
 
   return (
     <div className="App">
-      <div className="post">
-        <div className="post_content">
-          <strong>1. Golang</strong>
-          <div>
-            Golang - тема
-          </div>
-        </div>
-        <div className="post_btns">
-          <button>delete</button>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="post_content">
-          <strong>1. Golang</strong>
-          <div>
-            Golang - тема
-          </div>
-        </div>
-        <div className="post_btns">
-          <button>delete</button>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="post_content">
-          <strong>1. Golang</strong>
-          <div>
-            Golang - тема
-          </div>
-        </div>
-        <div className="post_btns">
-          <button>delete</button>
-        </div>
-      </div>
+        <PostItem post={{id: 1, title: 'Golang', body: 'Description'}}/>
+        <PostItem post={{id: 2, title: 'Python', body: 'Description'}}/>
+        <PostItem post={{id: 3, title: 'JavaScript', body: 'Description'}}/>
     </div>
   );
 }
